2a458ce1a70aaf512a83517c44a4b8607c529df1
Bjoern Kerler <info@revskills.de>
2022-08-26 22:13:07 +0200

    Adapt to PEP8 and fix some bugs

--- python/tpms_poore/decoder.py
+++ python/tpms_poore/decoder.py
@@ -23,25 +23,26 @@ import numpy
 from gnuradio import gr
 import pmt
 
+
 class decoder(gr.sync_block):
     """
     docstring for block decoder
     """
+
     def __init__(self):
         gr.sync_block.__init__(self,
-            name="decoder",
-            in_sig=[numpy.float32],
-            out_sig=[])
-            
+                               name="decoder",
+                               in_sig=[numpy.float32],
+                               out_sig=[])
+
         self.start_tag = 0
         self.end_tag = 0
         self.if_data = numpy.array([], dtype=numpy.float32)
         self.do_analysis = False
         self.message_number = 1
-        
-        self.message_port_register_out(pmt.intern('out'))
-        self.message_port_register_out(pmt.intern("bytes"))        
 
+        self.message_port_register_out(pmt.intern('out'))
+        self.message_port_register_out(pmt.intern("bytes"))
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
@@ -53,12 +54,12 @@ class decoder(gr.sync_block):
         tags = self.get_tags_in_window(0, 0, in0_len, pmt.string_to_symbol("burst"))
 
         # Tag Exists
-        for tag in tags:  #.offset, .key, .value    
-                    
+        for tag in tags:  # .offset, .key, .value
+
             # Record on Start
             if str(tag.value) == "#t":
                 self.start_tag = tag.offset
-                self.if_data = numpy.append(self.if_data, in0[self.start_tag-window_start:])
+                self.if_data = numpy.append(self.if_data, in0[self.start_tag - window_start:])
 
             # Stop Recording on Stop
             if str(tag.value) == "#f":
@@ -66,17 +67,17 @@ class decoder(gr.sync_block):
                 burst_size = self.end_tag - self.start_tag
 
                 # Perfect Size
-                if burst_size > 600 and burst_size < 900: 
+                if 600 < burst_size < 900:
                     self.do_analysis = True
 
                     # Multiple Windows
                     if len(self.if_data) > 0:
-                        self.if_data = numpy.append(self.if_data, in0[:self.end_tag-window_start])
+                        self.if_data = numpy.append(self.if_data, in0[:self.end_tag - window_start])
 
                     # One Window
                     else:
-                        self.if_data = in0[self.start_tag-window_start:self.end_tag-window_start]
-                        
+                        self.if_data = in0[self.start_tag - window_start:self.end_tag - window_start]
+
                 # Ignore and Reset
                 else:
                     self.start_tag = 0
@@ -84,14 +85,14 @@ class decoder(gr.sync_block):
                     self.if_data = numpy.array([], dtype=numpy.float32)
 
         # Whole Window with no Stop Tag
-        if len(tags) == 0 and len(self.if_data) < 900 and len(self.if_data) > 0:
+        if len(tags) == 0 and 900 > len(self.if_data) > 0:
             self.if_data = numpy.append(self.if_data, in0)
 
         # Do Analysis on all the Data
         if self.do_analysis is True:
-            
-            #print "DOING ANALYSIS!!"
-            #print len(self.if_data)
+
+            # print "DOING ANALYSIS!!"
+            # print len(self.if_data)
 
             # Obtain Bitstream
             get_bits = self.getBitstream()
@@ -103,7 +104,6 @@ class decoder(gr.sync_block):
                 # Print to Output Port
                 self.message_port_pub(pmt.intern("out"), pmt.to_pmt(get_message))
 
-
             # Reset
             self.start_tag = 0
             self.end_tag = 0
@@ -112,26 +112,24 @@ class decoder(gr.sync_block):
 
         return in0_len
 
-
     def getBitstream(self):
         """ Prints out the bitstream from the instantaneous frequency.
         """
         try:
             bitstream = "-1"
-            
+
             # Find the Start
-            found_start = False
-            start_loc1 = -1
             start_loc2 = -1
-            for n in range(20,len(self.if_data)):
-                if all(m == 1 for m in self.if_data[n-20:n-10]) and all(mm == 0 for mm in self.if_data[n-10:n-2]):
+            for n in range(20, len(self.if_data)):
+                if all(m == 1 for m in self.if_data[n - 20:n - 10]) and all(
+                        mm == 0 for mm in self.if_data[n - 10:n - 2]):
                     start_loc1 = n
-                    
+
                     # Find the Start of the First '1'                    
-                    for n in range(0,100):
-                        if self.if_data[start_loc1+n] == 1:
-                            start_loc2 = start_loc1 + n
-                            break  
+                    for h in range(0, 100):
+                        if self.if_data[start_loc1 + h] == 1:
+                            start_loc2 = start_loc1 + h
+                            break
                     break
 
             # Produce the Bitstream
@@ -139,14 +137,14 @@ class decoder(gr.sync_block):
                 bitstream = '0'
                 prev_seq = 0
                 prev_loc = start_loc2
-                
+
                 # Data Start to End of Tagged Message
-                for n in range(start_loc2,len(self.if_data)):
-                    
+                for n in range(start_loc2, len(self.if_data)):
+
                     # Find the Next Change
                     if int(self.if_data[n]) == prev_seq:
-                        point_diff = n-prev_loc
-                        
+                        point_diff = n - prev_loc
+
                         # One Manchester Encoded Bit
                         if point_diff < 7:
                             if self.if_data[n] == 0:
@@ -156,7 +154,7 @@ class decoder(gr.sync_block):
                                 bitstream = bitstream + '0'
                                 prev_seq = 0
                             prev_loc = n
-                            
+
                         # Two Manchester Encoded Bits
                         elif (point_diff > 7) and (point_diff < 25):
                             if self.if_data[n] == 0:
@@ -166,59 +164,60 @@ class decoder(gr.sync_block):
                                 bitstream = bitstream + '00'
                                 prev_seq = 0
                             prev_loc = n
-                            
+
                         # Reached the End
                         else:
                             break
-            
+
             # Pad Some Extra
-            bitstream = bitstream + 5*bitstream[-1]
-                        
+            bitstream = bitstream + 5 * bitstream[-1]
+
         # Error     
         except:
             bitstream = "-1"
 
-        return bitstream        
-        
+        return bitstream
+
     def decodeBitstream(self, get_bits):
         """ Manchester decodes the bitstream and diffs the bits to produce a message.
-        """        
+        """
         # Manchester Decode the Bits
         get_dec_man = ''
-        for m in range(0,len(get_bits),2):
-            if get_bits[m:m+2] == '01':
+        for m in range(0, len(get_bits), 2):
+            if get_bits[m:m + 2] == '01':
                 get_dec_man = get_dec_man + '0'
-            elif get_bits[m:m+2] == '10':
+            elif get_bits[m:m + 2] == '10':
                 get_dec_man = get_dec_man + '1'
             else:
                 get_dec_man = get_dec_man + '?'
-                
+
         # Differentiate the Decoded Bits
         get_diff = ''
-        if len(get_dec_man) > 1:                    
+        if len(get_dec_man) > 1:
             # Differentiate
-            for b in range(1,len(get_dec_man)):
-                if get_dec_man[b-1] == get_dec_man[b]:
+            for b in range(1, len(get_dec_man)):
+                if get_dec_man[b - 1] == get_dec_man[b]:
                     get_diff = get_diff + '0'
                 else:
-                    get_diff = get_diff + '1'                    
+                    get_diff = get_diff + '1'
 
-        # Generate Output Message
+                    # Generate Output Message
+        msg = ""
         if len(get_diff) >= 66:
-            sensor_id = '%.*X' % (2, int('0b'+get_diff[0:28], 0))
+            sensor_id = '%.*X' % (2, int('0b' + get_diff[0:28], 0))
             battery_status = get_diff[28]
             counter = get_diff[29:31]
             unknown1 = get_diff[31]
             unknown2 = get_diff[32]
             self_test = get_diff[33]
             tire_pressure = get_diff[34:42]
-            tp_psi = str((int(tire_pressure,2)-40)*.363)
+            tp_psi = str((int(tire_pressure, 2) - 40) * .363)
             tp_complement = get_diff[42:50]
             tire_temp = get_diff[50:58]
-            temp_celsius = str(int(tire_temp,2)-40)
-            temp_fahr = str((int(temp_celsius)*9/5)+32)
+            temp_celsius = str(int(tire_temp, 2) - 40)
+            temp_fahr = str((int(temp_celsius) * 9 / 5) + 32)
             crc = get_diff[58:66]
-            
+
             msg = "Message #" + str(self.message_number) + ":\n" + \
                   "Bitstream: " + get_bits + "\n" + \
                   "Decoded Bits: " + get_diff + "\n" + \
@@ -230,22 +229,18 @@ class decoder(gr.sync_block):
                   "Self Test Failed: " + self_test + "\n" + \
                   "Tire Pressure: " + tire_pressure + " | PSI: " + tp_psi + "\n" + \
                   "T.P. Complement: " + tp_complement + "\n" + \
-                  "Tire Temperature: " + tire_temp + " | Celsius: " + temp_celsius + " | Fahrenheit: " + temp_fahr + "\n" + \
+                  "Tire Temperature: " + tire_temp + " | Celsius: " + temp_celsius + " | Fahrenheit: " + \
+                  temp_fahr + "\n" + \
                   "CRC: " + crc + "\n"
-                  
+
             self.message_number = self.message_number + 1
-            
+
             # Add Excess Bits
-            if len(get_diff)%8 != 0:
-                get_diff = get_diff + '0'*(8-len(get_diff)%8)
-            
+            if len(get_diff) % 8 != 0:
+                get_diff = get_diff + '0' * (8 - len(get_diff) % 8)
+
             # Print Bytes to Output Port    
-            data_hex = ('%0*X' % (2,int(get_diff,2))).zfill(len(get_diff)/4).decode("hex")
+            data_hex = ('%0*X' % (2, int(get_diff, 2))).zfill(len(get_diff) // 4).decode("hex")
             self.message_port_pub(pmt.intern("bytes"), pmt.to_pmt(data_hex))
 
         return msg
-                
-                
-                
-                
-                
--- python/tpms_poore/message_generator_pdu.py
+++ python/tpms_poore/message_generator_pdu.py
@@ -27,6 +27,7 @@ import threading
 import crcmod
 import sys
 
+
 class message_generator_pdu(gr.sync_block):
     """
     From github.com/xnk/pacific-tpms - Werner Johansson
@@ -71,14 +72,16 @@ class message_generator_pdu(gr.sync_block):
         Truncated polynomial 19, init value 0
         
     """
-    def __init__(self, repetition_interval, configuration, sensor_id, battery_status, counter, unknown1, unknown2, self_test, tire_pressure, tire_temp):
+
+    def __init__(self, repetition_interval, configuration, sensor_id, battery_status, counter, unknown1, unknown2,
+                 self_test, tire_pressure, tire_temp):
         gr.sync_block.__init__(self,
-            name="message_generator_pdu",
-            in_sig=None,
-            out_sig=None)
-        
+                               name="message_generator_pdu",
+                               in_sig=None,
+                               out_sig=None)
+
         self.message_port_register_out(pmt.intern('out'))
-        
+
         # Default Values
         self.repetition_interval = repetition_interval
         self.configuration = configuration
@@ -90,78 +93,81 @@ class message_generator_pdu(gr.sync_block):
         self.self_test = self_test
         self.tire_pressure = tire_pressure
         self.tire_temp = tire_temp
-        
+
         # Run in a Thread
         c_thread = threading.Thread(target=self.generateMessage, args=())
         c_thread.daemon = True
         c_thread.start()
 
-
-    
     def generateMessage(self):
         while True:
             print("GENERATE MESSAGE")
-                       
+
             # Access Code
-            access_code = '0000001111110'  #'000000111111001', some say the access code is: '00111111001' but the last '01' must translate to a manchester encoded '0' in the sensor id for all the fields to align
-            
+            # '000000111111001', some say the access code is: '00111111001' but the last '01' must translate to a
+            # manchester encoded '0' in the sensor id for all the fields to align
+            access_code = '0000001111110'
+
             # Sensor ID
-            #sensor_id = '0101001010001010010100010000'  # 528A510
-            sensor_id = bin(int(self.sensor_id,16))[2:].zfill(28)
-            
+            # sensor_id = '0101001010001010010100010000'  # 528A510
+            sensor_id = bin(int(self.sensor_id, 16))[2:].zfill(28)
+
             # Battery Status
-            #battery_status = '1'  # nothing when set to 0 or 1, might be part of counter, observed both 0 and 1 from the same sensor
+            # nothing when set to 0 or 1, might be part of counter, observed both 0 and 1 from the same sensor
+            # battery_status = '1'
             battery_status = self.battery_status
-            
+
             # Counter
-            #counter = '00'  # nothing when set to all values, observed counter-like behavior for these bits
+            # counter = '00'  # nothing when set to all values, observed counter-like behavior for these bits
             counter = self.counter
-            
+
             # Unknown1
-            #unknown1 = '0'  # nothing when set to 0 or 1, always 0
+            # unknown1 = '0'  # nothing when set to 0 or 1, always 0
             unknown1 = self.unknown1
-            
+
             # Unknown2
-            #unknown2 = '0'  # nothing when set to 0 or 1, always 0
+            # unknown2 = '0'  # nothing when set to 0 or 1, always 0
             unknown2 = self.unknown2
-            
+
             # Self-Test Failed
-            #self_test_failed = '0'  # Makes light flash when set to 1, always 0
+            # self_test_failed = '0'  # Makes light flash when set to 1, always 0
             self_test_failed = self.self_test
-            
+
             # Tire Pressure
-            #tire_pressure = '10101111'  # 49
-            #tire_pressure = '01101110'  # light starting threshold = 01101110 = 110 = (110-40)*.363 = 25.41
-            #tire_pressure = '11111111'
-            tire_pressure = "{0:08b}".format(int((self.tire_pressure/0.363)+40))
-            
+            # tire_pressure = '10101111'  # 49
+            # tire_pressure = '01101110'  # light starting threshold = 01101110 = 110 = (110-40)*.363 = 25.41
+            # tire_pressure = '11111111'
+            tire_pressure = "{0:08b}".format(int((self.tire_pressure / 0.363) + 40))
+
             # Tire Pressure Complement
-            #tire_pressure_complement = '01010000'           
-            #tire_pressure_complement = '10010001'
-            #tire_pressure_complement = '00000000'
+            # tire_pressure_complement = '01010000'
+            # tire_pressure_complement = '10010001'
+            # tire_pressure_complement = '00000000'
             tire_pressure_complement = ''.join('1' if x == '0' else '0' for x in tire_pressure)
-            
+
             # Tire Temperature
-            #tire_temperature = '01111010'  # Nothing happens when set to 00000000-11111111  
+            # tire_temperature = '01111010'  # Nothing happens when set to 00000000-11111111
             tire_temperature = "{0:08b}".format(int(self.tire_temp + 40))
 
             # CRC
-            #crc = '00001101'
-            bits_no_crc = sensor_id + battery_status + counter + unknown1 + unknown2 + self_test_failed + tire_pressure + tire_pressure_complement + tire_temperature
-            crc_data =  '000000' + bits_no_crc + ''
+            # crc = '00001101'
+            bits_no_crc = sensor_id + battery_status + counter + unknown1 + unknown2 + self_test_failed + \
+                tire_pressure + tire_pressure_complement + tire_temperature
+            crc_data = '000000' + bits_no_crc + ''
             crc_data_bytes = []
-            for n in range(0,len(crc_data)/8):
-                crc_data_bytes.append(int(crc_data[n*8:n*8+8],2))
-            crc_data_bytes = str(bytearray(crc_data_bytes))            
+            for n in range(0, len(crc_data) // 8):
+                crc_data_bytes.append(int(crc_data[n * 8:n * 8 + 8], 2))
+            crc_data_bytes = str(bytearray(crc_data_bytes))
             check_fn = crcmod.mkCrcFun(0x100 | 0x13, initCrc=0x0, rev=False)
             crc = '{0:08b}'.format(check_fn(crc_data_bytes))
-            
+
             # Assemble the Message
-            get_msg = sensor_id + battery_status + counter + unknown1 + unknown2 + self_test_failed + tire_pressure + tire_pressure_complement + tire_temperature + crc
-            
+            get_msg = sensor_id + battery_status + counter + unknown1 + unknown2 + self_test_failed + tire_pressure + \
+                tire_pressure_complement + tire_temperature + crc
+
             # UnDiff
             get_undiff = '0'
-            for b in range(0,len(get_msg)):
+            for b in range(0, len(get_msg)):
                 # Change
                 if get_msg[b] == '1':
                     if get_undiff[-1] == '0':
@@ -173,31 +179,32 @@ class message_generator_pdu(gr.sync_block):
                     if get_undiff[-1] == '0':
                         get_undiff = get_undiff + '0'
                     else:
-                        get_undiff = get_undiff + '1'            
-            
-            # Manchester Encode       
+                        get_undiff = get_undiff + '1'
+
+                        # Manchester Encode
             get_man = ''
             for m in get_undiff:
                 if m == '0':
                     get_man = get_man + '01'
                 else:
                     get_man = get_man + '10'
-                    
+
             # Assemble
-            data = access_code + get_man + '00000'  # there are always a few extra bits, next line needs a round number too
+            # there are always a few extra bits, next line needs a round number too
+            data = access_code + get_man + '00000'
             hex_data = '%0*X' % ((len(data) + 0) // 4, int(data, 2))  # 03F2D2B4AB4AD2AD555554D2CD4B54CD4CB2A0
             data = hex_data.decode('hex')  # Python 2
-            #data = bytes.fromhex(hex_string)  # Python 3
-                        
+            # data = bytes.fromhex(hex_string)  # Python 3
+
             # Send the PDU
             car = pmt.make_dict()
-            #data = '\x03\xF2\xD2\xB4\xAB\x4A\xD2\xAD\x54\xD5\x52\xD3\x34\xB5\x54\xCD\x2A\xAC\xB0\x00'  # real data
-            #data = '\xFF\xF2\xD2\xB4\xAB\x4A\xD2\xAD\x54\xD5\x52\xD3\x34\xB5\x54\xCD\x2A\xAC\x00\x00'  # delay test
-            #data = bytes(data)
+            # data = '\x03\xF2\xD2\xB4\xAB\x4A\xD2\xAD\x54\xD5\x52\xD3\x34\xB5\x54\xCD\x2A\xAC\xB0\x00'  # real data
+            # data = '\xFF\xF2\xD2\xB4\xAB\x4A\xD2\xAD\x54\xD5\x52\xD3\x34\xB5\x54\xCD\x2A\xAC\x00\x00'  # delay test
+            # data = bytes(data)
             data = numpy.frombuffer(data, dtype=numpy.uint8)
             cdr = pmt.to_pmt(data)
-            pdu = pmt.cons(car, cdr)            
-            
+            pdu = pmt.cons(car, cdr)
+
             try:
                 self.message_port_pub(pmt.intern('out'), pdu)
             except:
@@ -205,4 +212,3 @@ class message_generator_pdu(gr.sync_block):
 
             # Sleep and Repeat
             time.sleep(self.repetition_interval)
-
